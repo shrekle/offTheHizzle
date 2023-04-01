a662e31e1da7ae030fdf16c1bb4652c28451aac0
shrekle <nanonline760@gmail.com>
2023-04-01 12:21:02 -0800

    Initial Commit

---
+++ offTheHizzle/offTheHizzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  offTheHizzleApp.swift
+//  offTheHizzle
+//
+//  Created by adrian garcia on 4/1/23.
+//
+
+import SwiftUI
+
+@main
+struct offTheHizzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
